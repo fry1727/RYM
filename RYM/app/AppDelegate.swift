3dd0ce4ace26cbdd6ab865707953a72e43465f5f
fry1727 <Fry1727@gmail.com>
2022-09-28 12:07:11 +0200

    add search Bar to remainder List

--- RYM/app/AppDelegate.swift
+++ RYM/app/AppDelegate.swift
@@ -13,26 +13,30 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var coreDataStack = CoreDataStack()
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
                      [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
+        
         let window = UIWindow(frame: UIScreen.main.bounds)
         let navigationController = NavigationController()
-
+        
         let launchView = LaunchScreenView(navigationController: navigationController, viewContext: coreDataStack.persistentContainer)
-
+        
         navigationController.setViewControllers([UIHostingController(rootView: launchView)], animated: false)
         navigationController.navigationBar.isHidden = true
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
         self.window = window
-
+        
         Notifications.shared.requestAutorization()
-
+        
+        //MARK: - Customizing SearchBar
+        UISearchBar.appearance().tintColor = .black
+        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).backgroundColor = .black
+        
         return true
     }
-/// For update notification permissin status  
+    /// For update notification permissin status  
     func applicationDidBecomeActive(_ application: UIApplication) {
         Notifications.shared.permissionGranted { granted in
             AppConfig.shared.notificationAccess = granted
